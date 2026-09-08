7f78ebe2a38defc934805f9e222207514251e22d
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-07 21:55:09 -0300

    commit incial

---
+++ teste/src/test/java/com/ekan/teste/TesteApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ekan.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
