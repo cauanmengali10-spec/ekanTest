3809559c9603cac87e1802804b7e929976ea5376
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-08 21:56:42 -0300

    feat: cria domain beneficiario

---
+++ teste/src/main/java/com/ekan/teste/beneficiario/domain/Beneficiario.java
@@ -0,0 +1,60 @@
+package com.ekan.teste.beneficiario.domain;
+
+
+import com.ekan.teste.documento.domain.Documento;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "beneficiario")
+public class Beneficiario {
+
+    @Id
+    @GeneratedValue
+    private UUID idBeneficiario;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @NotNull
+    @OneToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            mappedBy = "beneficiario", orphanRemoval = true)
+    private List<Documento> documento;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false)
+    private LocalDateTime dataInclusao;
+
+    @Column(nullable = false)
+    private LocalDateTime dataAtualizacao;
+
+    @PrePersist
+    protected void onCreate() {
+        this.dataInclusao = LocalDateTime.now();
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+}
