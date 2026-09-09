d47f2a1d546d6e7028204c3ba46797a4039eed9b
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-08 21:56:13 -0300

    feat: implementa handler

---
+++ teste/src/main/java/com/ekan/teste/handler/ErrorCode.java
@@ -0,0 +1,28 @@
+package com.ekan.teste.handler;
+
+public enum ErrorCode {
+  BENEFICIARIO_NAO_ENCONTRADO("beneficiario.nao.encontrado"),
+  BENEFICIARIO_JA_EXISTE("beneficiario.ja.existe"),
+  BENEFICIARIO_INVALIDO("beneficiario.invalido"),
+  BENEFICIARIO_NOME_INVALIDO("beneficiario.nome.invalido"),
+  BENEFICIARIO_CPF_INVALIDO("beneficiario.cpf.invalido"),
+  BENEFICIARIO_CPF_JA_EXISTE("beneficiario.cpf.ja.existe"),
+  BENEFICIARIO_DATA_NASCIMENTO_INVALIDA("beneficiario.data.nascimento.invalida"),
+  BENEFICIARIO_TELEFONE_INVALIDO("beneficiario.telefone.invalido"),
+  DOCUMENTO_NAO_ENCONTRADO("documento.nao.encontrado"),
+  DOCUMENTO_JA_EXISTE("documento.ja.existe"),
+  DOCUMENTO_INVALIDO("documento.invalido"),
+  DOCUMENTO_TIPO_INVALIDO("documento.tipo.invalido"),
+  DOCUMENTO_DATA_EXPIRACAO_INVALIDA("documento.data.expiracao.invalida");
+
+
+  private final String code;
+
+  ErrorCode(String code) {
+    this.code = code;
+  }
+
+  public String getCode() {
+    return code;
+  }
+}
