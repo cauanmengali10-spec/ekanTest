7622f059ed2092c526a3f6759c061c4f3ab3e1ce
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-08 21:59:32 -0300

    feat: implementa domain documento pertencente ao beneficiario

---
+++ teste/src/main/java/com/ekan/teste/documento/domain/Documento.java
@@ -0,0 +1,53 @@
+package com.ekan.teste.documento.domain;
+
+import com.ekan.teste.beneficiario.domain.Beneficiario;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "documento")
+public class Documento {
+
+    @Id
+    @GeneratedValue
+    private UUID idDocumento;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_beneficiario")
+    private Beneficiario beneficiario;
+
+    @Column(nullable = false)
+    private String tipoDocumento;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private LocalDateTime dataInclusao;
+
+    @Column(nullable = false)
+    private LocalDateTime dataAtualizacao;
+
+    @PrePersist
+    protected void onCreate(){
+        this.dataInclusao = LocalDateTime.now();
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+}
