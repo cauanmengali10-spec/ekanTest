3713c0c5e28be1dfbbd6ab8a9c6aa81a8bed3ee3
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-08 22:00:30 -0300

    feat: inicia camadas de servico de aplicação

---
+++ teste/src/main/java/com/ekan/teste/beneficiario/service/BeneficiarioApplicationService.java
@@ -0,0 +1,14 @@
+package com.ekan.teste.beneficiario.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class BeneficiarioApplicationService {
+
+
+}
---
+++ teste/src/main/java/com/ekan/teste/beneficiario/service/BeneficiarioService.java
@@ -0,0 +1,4 @@
+package com.ekan.teste.beneficiario.service;
+
+public interface BeneficiarioService {
+}
