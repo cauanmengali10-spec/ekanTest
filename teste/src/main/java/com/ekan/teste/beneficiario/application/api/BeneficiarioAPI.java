c2ec0f5b5dbc0faa627c41a9c1b1f0df6913c3b5
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-08 21:57:49 -0300

    feat: implementa camada de api

---
+++ teste/src/main/java/com/ekan/teste/beneficiario/application/api/BeneficiarioAPI.java
@@ -0,0 +1,18 @@
+package com.ekan.teste.beneficiario.application.api;
+
+import com.ekan.teste.beneficiario.application.api.request.BeneficiarioRequest;
+import com.ekan.teste.beneficiario.application.api.response.BeneficiarioResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/beneficiario")
+public interface BeneficiarioAPI {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    BeneficiarioResponse criaBeneficiario(BeneficiarioRequest beneficiarioRequest);
+}
