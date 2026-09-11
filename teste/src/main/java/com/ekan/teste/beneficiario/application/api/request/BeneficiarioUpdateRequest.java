4a3e738308bf0e0070ce3175a65473d5eeb335ba
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-10 22:55:43 -0300

    feat: implementado dto de update para validar dados que serão inseridos na requisição

--- teste/src/main/java/com/ekan/teste/beneficiario/application/api/request/BeneficiarioUpdateRequest.java
+++ teste/src/main/java/com/ekan/teste/beneficiario/application/api/request/BeneficiarioUpdateRequest.java
@@ -1,9 +1,8 @@
 package com.ekan.teste.beneficiario.application.api.request;
 
 import jakarta.validation.constraints.*;
-import lombok.*;
-
 import java.time.LocalDate;
+import lombok.*;
 
 @Builder
 @NoArgsConstructor
@@ -12,14 +11,10 @@ import java.time.LocalDate;
 @Getter
 public class BeneficiarioUpdateRequest {
 
-    @NotBlank(message = "Nome é obrigatório") @Size(max = 255)
-    private String nome;
+  @NotBlank(message = "Nome é obrigatório") @Size(max = 255) private String nome;
 
-    @NotBlank(message = "Telefone é obrigatório")
-    @Pattern(regexp = "\\+?[0-9()\\-\\s]+", message = "Telefone inválido")
-    private String telefone;
+  @NotBlank(message = "Telefone é obrigatório") @Pattern(regexp = "\\+?[0-9()\\-\\s]+", message = "Telefone inválido")
+  private String telefone;
 
-    @NotNull(message = "Data de nascimento é obrigatória")
-    @PastOrPresent(message = "Data de nascimento deve ser no passado ou hoje")
-    private LocalDate dataNascimento;
+  @NotNull(message = "Data de nascimento é obrigatória") @PastOrPresent(message = "Data de nascimento deve ser no passado ou hoje") private LocalDate dataNascimento;
 }
