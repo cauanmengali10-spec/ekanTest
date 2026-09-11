95973c234fab5e69d6002999c336d72a4e4e72b5
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-10 22:51:56 -0300

    feat: implementado regra de negocio

--- teste/src/main/java/com/ekan/teste/beneficiario/service/BeneficiarioApplicationService.java
+++ teste/src/main/java/com/ekan/teste/beneficiario/service/BeneficiarioApplicationService.java
@@ -1,18 +1,18 @@
 package com.ekan.teste.beneficiario.service;
 
 import com.ekan.teste.beneficiario.application.api.request.BeneficiarioRequest;
+import com.ekan.teste.beneficiario.application.api.request.BeneficiarioUpdateRequest;
 import com.ekan.teste.beneficiario.application.api.response.BeneficiarioResponse;
 import com.ekan.teste.beneficiario.domain.Beneficiario;
 import com.ekan.teste.beneficiario.repository.BeneficiarioRepository;
 import com.ekan.teste.documento.domain.Documento;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -44,8 +44,20 @@ public class BeneficiarioApplicationService implements BeneficiarioService {
   @Transactional(readOnly = true)
   public Page<Documento> listarDocumentosDoBeneficiario(UUID idBeneficiario, Pageable pageable) {
     log.debug("[start] BeneficiarioApplicationService - listaDocumentosDoBeneficiario");
-    Page<Documento> documentos = beneficiarioRepository.listaDocumentosDoBeneficiario(idBeneficiario, pageable);
+    Page<Documento> documentos =
+        beneficiarioRepository.listaDocumentosDoBeneficiario(idBeneficiario, pageable);
     log.debug("[finish] BeneficiarioApplicationService - listaDocumentosDoBeneficiario");
     return documentos;
+  }
+
+  @Override
+  @Transactional
+  public BeneficiarioResponse atualizaBeneficiario(
+      UUID idBeneficiario, BeneficiarioUpdateRequest updateRequest) {
+    log.debug("[start] BeneficiarioApplicationService - atualizaBeneficiario");
+    Beneficiario beneficiario = beneficiarioRepository.buscaBeneficiarioPeloId(idBeneficiario);
+    beneficiario.atualizaBeneficiario(updateRequest);
+    log.debug("[finish] BeneficiarioApplicationService - atualizaBeneficiario");
+    return new BeneficiarioResponse(beneficiario);
   }
 }
