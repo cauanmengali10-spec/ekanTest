1675e34fbacd0eba044f1371f8345cfe7bcb2f3c
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-10 22:53:25 -0300

    fix: corrige espaçamento com spotless

--- teste/src/main/java/com/ekan/teste/beneficiario/infra/BeneficiarioJPARepository.java
+++ teste/src/main/java/com/ekan/teste/beneficiario/infra/BeneficiarioJPARepository.java
@@ -1,9 +1,8 @@
 package com.ekan.teste.beneficiario.infra;
 
 import com.ekan.teste.beneficiario.domain.Beneficiario;
-import java.util.UUID;
-
 import com.ekan.teste.documento.domain.Documento;
+import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -20,19 +19,18 @@ public interface BeneficiarioJPARepository
       countQuery = "SELECT count(b) FROM Beneficiario b")
   Page<UUID> buscarIds(Pageable pageable);
 
-
   @EntityGraph(attributePaths = "documento")
   @Query(
       value = "SELECT b FROM Beneficiario b ORDER BY b.nome ASC",
       countQuery = "SELECT count(distinct b) FROM Beneficiario b")
   Page<Beneficiario> buscarTodosBeneficiarios(Pageable pageable);
 
-
   @Query(
-          value = "SELECT d FROM Documento d "
-                  + "WHERE d.beneficiario.idBeneficiario = :id "
-                  + "ORDER BY d.dataInclusao ASC",
-          countQuery = "SELECT count(d) FROM Documento d "
-                  + "WHERE d.beneficiario.idBeneficiario = :id")
+      value =
+          "SELECT d FROM Documento d "
+              + "WHERE d.beneficiario.idBeneficiario = :id "
+              + "ORDER BY d.dataInclusao ASC",
+      countQuery =
+          "SELECT count(d) FROM Documento d " + "WHERE d.beneficiario.idBeneficiario = :id")
   Page<Documento> buscaDocumentosDoBeneficiario(@Param("id") UUID id, Pageable pageable);
 }
