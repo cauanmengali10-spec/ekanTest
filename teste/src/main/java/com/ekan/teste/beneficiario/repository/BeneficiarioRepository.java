d25e02b6ebf89571214a9be48e2e2ba6479cc2a1
Cauan Mengali <cauanmengali10@gmail.com>
2026-09-10 22:54:52 -0300

    feat: implementado contrato do repository

--- teste/src/main/java/com/ekan/teste/beneficiario/repository/BeneficiarioRepository.java
+++ teste/src/main/java/com/ekan/teste/beneficiario/repository/BeneficiarioRepository.java
@@ -2,10 +2,9 @@ package com.ekan.teste.beneficiario.repository;
 
 import com.ekan.teste.beneficiario.domain.Beneficiario;
 import com.ekan.teste.documento.domain.Documento;
+import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.UUID;
 
 public interface BeneficiarioRepository {
   Beneficiario salva(Beneficiario beneficiario);
@@ -13,4 +12,6 @@ public interface BeneficiarioRepository {
   Page<Beneficiario> listaTodosBeneficiarios(Pageable pageable);
 
   Page<Documento> listaDocumentosDoBeneficiario(UUID idBeneficiario, Pageable pageable);
+
+  Beneficiario buscaBeneficiarioPeloId(UUID idBeneficiario);
 }
